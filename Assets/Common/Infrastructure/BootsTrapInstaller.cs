ccda0958c057d3cc703a2c624d9260f0c3dc17d9
Efremov Danil <naturforscher@outlook.com>
2022-02-27 08:57:51 +0300

    Added ProjectContext and SceneContext + added BootsTrapInstaller

---
+++ Assets/Common/Infrastructure/BootsTrapInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Common.Infrastructure
+{
+    public class BootsTrapInstaller: MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
