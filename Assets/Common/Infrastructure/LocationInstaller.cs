b4f0312575cf9fefa7468ab34abc905829a5c720
Efremov Danil <naturforscher@outlook.com>
2022-02-27 10:31:10 +0300

    Added SceneContext + LocationInstaller base

---
+++ Assets/Common/Infrastructure/LocationInstaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Common.Infrastructure
+{
+    public class LocationInstaller: MonoInstaller
+    {
+        [SerializeField] private Transform _spawnLocation;
+        [SerializeField] private HeroController _playerPrefab;
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
